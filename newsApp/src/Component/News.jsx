5afaf86bd1861b31090c451f796cc883204ec5f6
Sintu4613S <shiva49767@gmail.com>
2026-04-20 21:11:48 +0530

    create two new file

---
+++ newsApp/src/Component/News.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is a simple news app</h1>
+
+      </div>
+    )
+  }
+}
+
+export default News

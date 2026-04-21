ed8296d9d7ea7967ca34b0c6afd8e83a70dac9ce
Sintu4613S <shiva49767@gmail.com>
2026-04-21 17:14:14 +0530

    Refactor NewsItem component to use props for title and description

--- newsApp/src/Component/NewsItem.jsx
+++ newsApp/src/Component/NewsItem.jsx
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let { title, desc, ImgUrl } = this.props;
     return (
       <div className='container my-3'>
         <div className="card" style={{ width: '18rem' }}>
-          {/* <img src="" className="card-img-top" alt="..."> */}
+          <img src={ImgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{desc}</p>
             <a href="/" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>

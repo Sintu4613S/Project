ce8ca8ece2173b9f707f750b3412d4bb558321df
Sintu4613S <shiva49767@gmail.com>
2026-04-25 11:17:50 +0530

    Update fallback image in NewsItem component for better user experience

--- newsApp/src/Component/NewsItem.jsx
+++ newsApp/src/Component/NewsItem.jsx
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
       <>
         <div className="container">
           <div className="card" style={{ width: '18rem' }}>
-            <img src={imgUrl ? imgUrl : 'https://images.indianexpress.com/2026/04/Sooryavanshi-6-1.jpg'} className="card-img-top" alt="img" />
+            <img src={imgUrl ? imgUrl : 'Loading....'} className="card-img-top" alt="img" />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{desc}</p>

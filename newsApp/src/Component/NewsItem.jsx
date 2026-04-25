3a9b266e34a0019aaae6e95e9f7126779de006e7
Sintu4613S <shiva49767@gmail.com>
2026-04-25 11:46:14 +0530

    Refactor NewsItem component for improved styling consistency

--- newsApp/src/Component/NewsItem.jsx
+++ newsApp/src/Component/NewsItem.jsx
@@ -6,12 +6,12 @@ export class NewsItem extends Component {
     return (
       <>
         <div className="container">
-          <div className="card" style={{ width: '18rem' }}>
+          <div className="card">
             <img src={imgUrl ? imgUrl : 'Loading....'} className="card-img-top" alt="img" />
-            <div className="card-body">
+            <div className="card-body text-center">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{desc}</p>
-              <a href={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark">Read More</a>
+              <a href={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark ">Read More</a>
             </div>
           </div>
         </div>

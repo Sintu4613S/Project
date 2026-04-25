2157cf331abacd86c35eadb34d042d6aa75d97b0
Sintu4613S <shiva49767@gmail.com>
2026-04-25 09:29:51 +0530

    Update NavBar component: add navigation links for various categories

--- newsApp/src/Component/NavBar.jsx
+++ newsApp/src/Component/NavBar.jsx
@@ -13,10 +13,30 @@ export class NavBar extends Component {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">Home</a>
+                  <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/business">Business</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">About Us</a>
+                  <a className="nav-link active" aria-current="page" href="/general">General</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/entertainment">Entertainment</a>
+                </li>
+
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/health">Health</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/science">Science</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/sports">Sports</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="/technology">Technology</a>
                 </li>
               </ul>
               {/* <form className="d-flex" role="search">

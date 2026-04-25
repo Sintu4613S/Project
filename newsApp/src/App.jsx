b1e06b3b746bf477a67e7f6168260f22ffdc723e
Sintu4613S <shiva49767@gmail.com>
2026-04-25 10:25:07 +0530

    Refactor App component to wrap routes with Routes and add navigation routes

--- newsApp/src/App.jsx
+++ newsApp/src/App.jsx
@@ -1,12 +1,22 @@
 import NavBar from './Component/NavBar'
 import './App.css'
 import News from './Component/News';
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <NavBar />
-      <News pageSize={10} category='general' />
+
+
+      <Routes>
+        <NavBar />
+        <Route exact path="/home" element={<News pageSize={10} category='general' />} />
+        <Route exact path="/general" element={<News pageSize={10} category='general' />} />
+        <Route exact path="/business" element={<News pageSize={10} category='business' />} />
+        <Route exact path="/entertainment" element={<News pageSize={10} category='health' />} />
+        <Route exact path="/science" element={<News pageSize={10} category='science' />} />
+        <Route exact path="/sports" element={<News pageSize={10} category='sports' />} />
+        <Route exact path="/technologyl" element={<News pageSize={10} category='technology' />} />
+      </Routes>
     </>
   );
 }

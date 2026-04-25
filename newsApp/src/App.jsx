a295cb65ed63ee88d88747aaafc65854c3d56778
Sintu4613S <shiva49767@gmail.com>
2026-04-25 11:34:02 +0530

    Fix route paths for entertainment and health categories in App component

--- newsApp/src/App.jsx
+++ newsApp/src/App.jsx
@@ -9,10 +9,10 @@ function App() {
       <NavBar />
       <Routes>
         <Route key="general" exact path="/" element={<News pageSize={10} category='general' />} />
-        <Route key="general" exact path="/general" element={<News pageSize={10} category='general' />} />
         <Route key="business" exact path="/business" element={<News pageSize={10} category='business' />} />
         <Route key="general" exact path="/general" element={<News pageSize={10} category='general' />} />
-        <Route key="health" exact path="/entertainment" element={<News pageSize={10} category='health' />} />
+        <Route key="general" exact path="/entertainment" element={<News pageSize={10} category='entertainment' />} />
+        <Route key="health" exact path="/health" element={<News pageSize={10} category='health' />} />
         <Route key="science" exact path="/science" element={<News pageSize={10} category='science' />} />
         <Route key="sports" exact path="/sports" element={<News pageSize={10} category='sports' />} />
         <Route key="technology" exact path="/technology" element={<News pageSize={10} category='technology' />} />

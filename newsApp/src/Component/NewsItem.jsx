fe5a8d700473c2e2e0119756d6b30ce263272227
Sintu4613S <shiva49767@gmail.com>
2026-04-28 22:46:44 +0530

    Refactor NewsItem component to destructure props for cleaner code

--- newsApp/src/Component/NewsItem.jsx
+++ newsApp/src/Component/NewsItem.jsx
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 const NewsItem = (props) => {
+  let { title, desc, imgUrl, url } = props;
   return (
     <>
       <div className="container">
         <div className="card" style={{ minHeight: '400px' }}>
-          <img src={props.imgUrl} className="card-img-top" alt="img" />
+          <img src={imgUrl} className="card-img-top" alt="img" />
           <div className="card-body text-center" >
-            <h5 className="card-title">{props.title}</h5>
-            <p className="card-text">{props.desc}</p>
-            <Link to={props.url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark ">Read More</Link>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{desc}</p>
+            <Link to={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark ">Read More</Link>
           </div>
         </div>
       </div>

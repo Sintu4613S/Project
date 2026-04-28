42ac683249a23142f7285425176bb9661cb67901
Sintu4613S <shiva49767@gmail.com>
2026-04-28 21:03:35 +0530

    Refactor App component to use class-based structure and include apiKey for News component

--- newsApp/src/App.jsx
+++ newsApp/src/App.jsx
@@ -1,24 +1,30 @@
-import NavBar from './Component/NavBar'
+import React, { Component } from 'react'
 import './App.css'
-import News from './Component/News';
+import NavBar from './Component/NavBar'
+import News from './Component/News'
 import { Routes, Route } from "react-router-dom";
-function App() {
-  return (
-    <>
 
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<News key="general" pageSize={10} category='general' />} />
-        <Route exact path="/business" element={<News key="business" pageSize={10} category='business' />} />
-        <Route exact path="/general" element={<News key="general" pageSize={10} category='general' />} />
-        <Route exact path="/entertainment" element={<News key="entertainment" pageSize={10} category='entertainment' />} />
-        <Route exact path="/health" element={<News key="health" pageSize={10} category='health' />} />
-        <Route exact path="/science" element={<News key="science" pageSize={10} category='science' />} />
-        <Route exact path="/sports" element={<News key="sports" pageSize={10} category='sports' />} />
-        <Route exact path="/technology" element={<News key="technology" pageSize={10} category='technology' />} />
-      </Routes>
-    </>
-  );
+export class App extends Component {
+  apiKey = '8f5eefadd07341a8ac2f4b3c90a296ac'
+  render() {
+    return (
+      <div>
+        <>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<News apiKey={this.apiKey} key="general" pageSize={10} category='general' />} />
+            <Route exact path="/business" element={<News apiKey={this.apiKey} key="business" pageSize={10} category='business' />} />
+            <Route exact path="/general" element={<News apiKey={this.apiKey} key="general" pageSize={10} category='general' />} />
+            <Route exact path="/entertainment" element={<News apiKey={this.apiKey} key="entertainment" pageSize={10} category='entertainment' />} />
+            <Route exact path="/health" element={<News apiKey={this.apiKey} key="health" pageSize={10} category='health' />} />
+            <Route exact path="/science" element={<News apiKey={this.apiKey} key="science" pageSize={10} category='science' />} />
+            <Route exact path="/sports" element={<News apiKey={this.apiKey} key="sports" pageSize={10} category='sports' />} />
+            <Route exact path="/technology" element={<News apiKey={this.apiKey} key="technology" pageSize={10} category='technology' />} />
+          </Routes>
+        </>
+      </div>
+    )
+  }
 }
 
 export default App

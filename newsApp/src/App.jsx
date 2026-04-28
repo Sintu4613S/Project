47d5e76941fa8d028fb04dc4d2eb4bc7e54394c8
Sintu4613S <shiva49767@gmail.com>
2026-04-28 21:55:35 +0530

    Refactor News component to use pageSize from class property instead of hardcoded value

--- newsApp/src/App.jsx
+++ newsApp/src/App.jsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 
 export class App extends Component {
   apiKey = import.meta.env.VITE_NEWS_API
+  pageSize = 20
   // If you want to use a fallback key during development, you can uncomment the line below:
   // apiKey = import.meta.env.VITE_NEWS_API || '8f5eefadd07341a8ac2f4b3c90a296ac'
   render() {
@@ -14,14 +15,14 @@ export class App extends Component {
         <>
           <NavBar />
           <Routes>
-            <Route exact path="/" element={<News apiKey={this.apiKey} key="general" pageSize={10} category='general' />} />
-            <Route exact path="/business" element={<News apiKey={this.apiKey} key="business" pageSize={10} category='business' />} />
-            <Route exact path="/general" element={<News apiKey={this.apiKey} key="general" pageSize={10} category='general' />} />
-            <Route exact path="/entertainment" element={<News apiKey={this.apiKey} key="entertainment" pageSize={10} category='entertainment' />} />
-            <Route exact path="/health" element={<News apiKey={this.apiKey} key="health" pageSize={10} category='health' />} />
-            <Route exact path="/science" element={<News apiKey={this.apiKey} key="science" pageSize={10} category='science' />} />
-            <Route exact path="/sports" element={<News apiKey={this.apiKey} key="sports" pageSize={10} category='sports' />} />
-            <Route exact path="/technology" element={<News apiKey={this.apiKey} key="technology" pageSize={10} category='technology' />} />
+            <Route exact path="/" element={<News apiKey={this.apiKey} key="general" pageSize={this.pagesize} category='general' />} />
+            <Route exact path="/business" element={<News apiKey={this.apiKey} key="business" pageSize={this.pagesize} category='business' />} />
+            <Route exact path="/general" element={<News apiKey={this.apiKey} key="general" pageSize={this.pagesize} category='general' />} />
+            <Route exact path="/entertainment" element={<News apiKey={this.apiKey} key="entertainment" pageSize={this.pagesize} category='entertainment' />} />
+            <Route exact path="/health" element={<News apiKey={this.apiKey} key="health" pageSize={this.pagesize} category='health' />} />
+            <Route exact path="/science" element={<News apiKey={this.apiKey} key="science" pageSize={this.pagesize} category='science' />} />
+            <Route exact path="/sports" element={<News apiKey={this.apiKey} key="sports" pageSize={this.pagesize} category='sports' />} />
+            <Route exact path="/technology" element={<News apiKey={this.apiKey} key="technology" pageSize={this.pagesize} category='technology' />} />
           </Routes>
         </>
       </div>

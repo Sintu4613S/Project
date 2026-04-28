f9e2afbd9c73327a5bcc97785eb235bed3d94e4d
Sintu4613S <shiva49767@gmail.com>
2026-04-28 22:19:52 +0530

    Refactor NewsItem component to use functional component syntax

--- newsApp/src/Component/NewsItem.jsx
+++ newsApp/src/Component/NewsItem.jsx
@@ -1,25 +1,22 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-export class NewsItem extends Component {
-  render() {
-    let { title, desc, imgUrl, url } = this.props;
-    return (
-      <>
-        <div className="container">
-          <div className="card" style={{ minHeight: '400px' }}>
-            <img src={imgUrl} className="card-img-top" alt="img" />
-            <div className="card-body text-center" >
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{desc}</p>
-              <Link to={url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark ">Read More</Link>
-            </div>
+const NewsItem = (props) => {
+  return (
+    <>
+      <div className="container">
+        <div className="card" style={{ minHeight: '400px' }}>
+          <img src={props.imgUrl} className="card-img-top" alt="img" />
+          <div className="card-body text-center" >
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.desc}</p>
+            <Link to={props.url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-dark ">Read More</Link>
           </div>
         </div>
+      </div>
 
-      </>
-    )
-  }
+    </>
+  )
 }
 
 export default NewsItem

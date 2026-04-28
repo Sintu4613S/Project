9fbf7e305b6db5f044c7c107e108b1ab162a165c
Sintu4613S <shiva49767@gmail.com>
2026-04-28 22:13:47 +0530

    Refactor Loading component to use functional component syntax

--- newsApp/src/Component/Loading.jsx
+++ newsApp/src/Component/Loading.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
-export class Loading extends Component {
-  render() {
-    return (
-      <div className='text-center my-3'>
-        <img src="loading.gif" alt="Loading....." style={{ width: '80px' }} />
-      </div>
-    )
-  }
+const Loading = () => {
+
+  return (
+    <div className='text-center my-3'>
+      <img src="loading.gif" alt="Loading....." style={{ width: '80px' }} />
+    </div>
+  )
+
 }
 
 export default Loading

29ad2cbb86004e851bebf31c63921e4b94c01980
Sintu4613S <shiva49767@gmail.com>
2026-04-30 10:53:00 +0530

    Add User model schema with fields for first name, last name, email, password, date, and comments

---
+++ inotebook/BackEnd/modules/User.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from 'mongoose';
+//const Schema = mongoose;
+
+const UserSchema = new Schema({
+  Fname: {
+    type: String,
+    required: true,
+  },
+  Lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+
+  comments: [{
+    body: String,
+    date: Date
+  }],
+
+})
+
+const User = mongoose.model('User', UserSchema);
+export default User

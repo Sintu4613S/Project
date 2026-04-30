58b6d30d40283d1f3290de86509f5c32f6466e6c
Sintu4613S <shiva49767@gmail.com>
2026-04-30 11:08:37 +0530

    Add API routes for authentication and notes

--- inotebook/BackEnd/index.js
+++ inotebook/BackEnd/index.js
@@ -1,9 +1,13 @@
 import connectToMongo from "./connectDb.js";
 import express from "express";
+import authRoutes from './routes/auth.js'
+import noteRoutes from './routes/note.js'
 connectToMongo();
-
 const app = express()
 const port = 3000
+
+app.use('/api/auth', authRoutes);
+app.use('/api/note', noteRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello Sintu!')

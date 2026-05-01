c4c80285648314bfee76ec5bd7fabedf252eda87
Sintu4613S <shiva49767@gmail.com>
2026-05-01 13:57:03 +0530

    Update server port to 5000 and enhance user registration error handling

--- inotebook/BackEnd/index.js
+++ inotebook/BackEnd/index.js
@@ -4,7 +4,7 @@ import authRoutes from './routes/auth.js'
 import noteRoutes from './routes/note.js'
 connectToMongo();
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(express.json())
 
--- inotebook/BackEnd/routes/auth.js
+++ inotebook/BackEnd/routes/auth.js
@@ -10,23 +10,31 @@ router.post('/',
     body('email', 'Enter a Valid Email').isEmail(),
     body('password', 'Password mustbe atleast 8 characters').isLength({ min: 8, max: 16 })
   ],
-  (req, res) => {
+  async (req, res) => {
     //Errros that contains the validation errors from the above rules
     const errors = validationResult(req);
     // If there are errors, return bad request and the errors in json format
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    }).then(user => res.json(user))
-      .catch(err => {
-        console.log("error:", err)
-        res.json({ error: 'Please enter a Valid Value', message: err.message })
+    try {
+      let user = await User.findOne({ email: req.body.email })
+      if (user) {
+        return res.status(401).json({ error: "Sorry User With This Email is Already exist" })
       }
-      )
+
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      })
+      res.json(user)
+    }
+    catch (err) {
+      console.log(err.message)
+      res.status(500).send("Some Error Occured")
+    }
+
     //If there are no errors, create a new user and save it to the database
     // res.send("Data saved successfully" + '\n' + JSON.stringify(req.body, null, 2));
     // console.log(req.body)

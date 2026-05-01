0888cd20fd724d16d9521c8485fdb5331cd25c2c
Sintu4613S <shiva49767@gmail.com>
2026-05-01 23:16:47 +0530

    Refactor index.js & clarity in code style

--- inotebook/BackEnd/index.js
+++ inotebook/BackEnd/index.js
@@ -1,20 +1,21 @@
-import connectToMongo from "./connectDb.js";
-import express from "express";
+import express from 'express'
+import connectToMongo from './connectDb.js';
 import authRoutes from './routes/auth.js'
 import noteRoutes from './routes/note.js'
-connectToMongo();
+connectToMongo()
+
 const app = express()
 const port = 5000
 
 app.use(express.json())
 
-app.use('/api/auth', authRoutes);
-app.use('/api/note', noteRoutes);
+app.use('/api/auth', authRoutes)
+app.use('/api/note', noteRoutes)
 
 app.get('/', (req, res) => {
-  res.send('Hello Sintu!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })

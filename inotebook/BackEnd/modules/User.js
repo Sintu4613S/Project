1761e3ca59a23328aefda3e8b65ec994d7fbac55
Sintu4613S <shiva49767@gmail.com>
2026-05-06 11:49:09 +0530

    Add the Comment

--- inotebook/BackEnd/modules/User.js
+++ inotebook/BackEnd/modules/User.js
@@ -27,7 +27,9 @@ const UserSchema = new Schema({
   // }],
 
 })
+// this is the method to create the model for the user and we can use this model to create the user in the database and we can also use this model to find the user in the database and return the response to the client.
 const User = mongoose.model('User', UserSchema);
+// this is the method to create the index for the email field and we can use this index to find the user in the database and return the response to the client.
 User.createIndexes();
 export default User
 

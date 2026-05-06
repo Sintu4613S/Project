1de1f4cc49a5c989d2802eec723235e8fc558bae
Sintu4613S <shiva49767@gmail.com>
2026-05-06 11:47:50 +0530

    Add fetchuser middleware for JWT verification and error handling

--- inotebook/BackEnd/middleware/fetchuser.js
+++ inotebook/BackEnd/middleware/fetchuser.js
@@ -1,1 +1,25 @@
+/* global process */
 import jwt from 'jsonwebtoken'
+import dotenv from "dotenv"
+dotenv.config();
+
+const fetchuser = (req, res, next) => {
+  const token = req.header('auth-token');
+  if (!token) {
+    return res.status(401).send({ error: "Please Enter the Right Token Crecidental" })
+  }
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET_KEY)
+    req.user = data.newUser
+    next();
+
+  }
+  catch (error) {
+    console.log("JWT Verification Error:", error.message)
+    res.status(401).send({ error: "Please Enter the Right Token Crecidental" })
+  }
+
+}
+
+
+export default fetchuser

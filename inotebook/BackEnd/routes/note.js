b22f979cd5340ecefa93c8b017e4101b35aeeebf
Sintu4613S <shiva49767@gmail.com>
2026-05-07 11:21:18 +0530

    Fix update and delete note routes to ensure proper user authorization and response handling

--- inotebook/BackEnd/routes/note.js
+++ inotebook/BackEnd/routes/note.js
@@ -55,28 +55,44 @@ router.post('/getallnote', fetchuser,
     }
   })
 // Route 3: api path is http://localhost:5000/api/note/updatenote/:id  and we can use this api to update the note by id and return the response to the client. Login required 
-router.put('/updatenote:id', fetchuser, async (req, res) => {
-  try {
-    const { title, description, tag } = req.body
-    const newNote = {}
-    if (title) { newNote.title = title }
-    if (description) { newNote.tdescription = description }
-    if (tag) { newNote.tag = tag }
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+  const { title, description, tag } = req.body
+  // Create a newNote object and add the title description and tag to this object if they are exist in the request body and we can use this object to update the note in the database and return the response to the client.
+  const newNote = {}
+  if (title) { newNote.title = title }
+  if (description) { newNote.description = description }
+  if (tag) { newNote.tag = tag }
+  // console.log(newNote)
 
-    let note = await Note.findById(req.params.id)
-    if (!note) {
-      return req.status(404).send("Not Found")
-    } // if  upadated user id is not match  with user id
-    if (note.user.toString !== req.user.id) {
-      return req.status(500).send("Not Allowed to change Note")
-    }
-    note = Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
-    res.json(note)
+  // this is the method to find the note by id and return the response to the client. where req.param.id is the
+  // id of the note that we want to update and we can use this id to find the note in the database and return the response to the client.
+  let note = await Note.findById(req.params.id)
+  // console.log(req.params.id)
+  if (!note) {
+    return res.status(404).send("Not Found")
+  } // if  upadated user id is not match  with user id
+  // this is the method to check the user id that we receive from the client and the user id that we have in the database and if they are not match then return the response to the client.in whch note.user is the user id that we have in the database and req.user.id is the user id that we receive from the client and we can use this method to check the user id and return the response to the client.
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed")
   }
-  catch (error) {
-    res.status(401).send("Internal Server error")
+  // this is the method to find the note by id and update the note in the database and return the response to the client. where req.param.id is the id of the note that we want to update and we can use this id to find the note in the database and return the response to the client. $set is the method to set the newNote object that we have created above and { new: true } is the method to return the updated note in the response to the client.
+  note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+  res.json(note)
+})
 
+// Route 4: api path is http://localhost:5000/api/note/deletenote/:id  and we can use this api to delete the note by id and return the response to the client. Login required
+router.delete('/deletenote/:id', fetchuser, async (req, res) => {
+  // this is the method to find the note by id and return the response to the client. where req.param.id is the id of the note that we want to delete and we can use this id to find the note in the database and return the response to the client.
+  let note = await Note.findById(req.params.id)
+  if (!note) {
+    return res.status(404).send("Not Found")
   }
-
+  // this is the method to check the user id that we receive from the client and the user id that we have in the database and if they are not match then return the response to the client.in whch note.user is the user id that we have in the database and req.user.id is the user id that we receive from the client and we can use this method to check the user id and return the response to the client.
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Allowed")
+  }
+  // this is the method to find the note by id and delete the note from the database and return the response to the client. where req.param.id is the id of the note that we want to delete and we can use this id to find the note in the database and return the response to the client.
+  note = await Note.findByIdAndDelete(req.params.id)
+  res.json(note)
 })
 export default router

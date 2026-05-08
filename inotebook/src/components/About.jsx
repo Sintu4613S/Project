e4576f4cb2eb30185eb076a0c483ed05f50ba47a
Sintu4613S <shiva49767@gmail.com>
2026-05-08 23:13:14 +0530

    Update About component

--- inotebook/src/components/About.jsx
+++ inotebook/src/components/About.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from "react"
+import notecontext from "../context/Notes/noteContext"
 
 export default function About() {
+  const a = useContext(notecontext)
   return (
     <div>
-      <h1>This is a About </h1>
+      <h1>This is a About {a.name} </h1>
     </div>
   )
 }

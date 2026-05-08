6622f9f61b90ad9a0b3d7e0e6c3bdb3e7cffe47c
Sintu4613S <shiva49767@gmail.com>
2026-05-08 17:04:26 +0530

    Creatin a File

---
+++ inotebook/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to iNotebook</h1>
+      <p>Your personal note-taking app.</p>
+    </div>
+  )
+}

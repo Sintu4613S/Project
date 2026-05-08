bca4620463d476a62f77b77de2b3cbd4f4f0f740
Sintu4613S <shiva49767@gmail.com>
2026-05-08 21:52:18 +0530

    Reorder Navbar component placement within Routes in App.js

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
+// eslint-disable-next-line
+
+
+
 
 function App() {
   return (
     <>
-      <Navbar />
+
+
       <Routes>
+        <Navbar />
         <Route path="/home" exact element={<Home />} />
         <Route path="/about" exact element={<About />} />
       </Routes>
+
+
 
     </>
   );

14d9e662e684ed354393b7f7cd4c22fd651a1c0e
Sintu4613S <shiva49767@gmail.com>
2026-05-09 10:21:08 +0530

    Refactor the About component

--- inotebook/src/components/About.jsx
+++ inotebook/src/components/About.jsx
@@ -1,11 +1,11 @@
-//import React, { useContext } from "react"
-//import notecontext from "../context/Notes/noteContext"
+import React, { useContext } from "react"
+import notecontext from "../context/Notes/noteContext"
 
 export default function About() {
-  //const a = useContext(notecontext)
+  const a = useContext(notecontext)
   return (
     <div>
-      <h1>This is about</h1>
+      <h1>This is about {a.name}</h1>
     </div>
   )
 }

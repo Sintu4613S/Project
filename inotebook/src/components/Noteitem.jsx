d12d92fa9cd1623a15d63bd5a679e7e746dbe421
Sintu4613S <shiva49767@gmail.com>
2026-05-12 20:20:09 +0530

    Refactor Noteitem component to enhance layout and structure

--- inotebook/src/components/Noteitem.jsx
+++ inotebook/src/components/Noteitem.jsx
@@ -3,10 +3,27 @@ import React from 'react'
 const Noteitem = (props) => {
   const { note } = props
   return (
-    <div>
-      {note.title}
-      {note.description}
-    </div>
+    <>
+
+      <div className="col-md-3 ">
+        <div className="card my-3" >
+          <div className="card-body">
+            <h5 className="card-title">{note.title}</h5>
+            <p className="card-text">{note.description}</p>
+            <p className="card-text"><small className="text-muted">{note.tag}</small></p>
+
+            <span className="material-symbols-outlined icon">
+              delete
+            </span>
+            <span className="material-symbols-outlined icon">
+              edit_square
+            </span>
+
+          </div>
+
+        </div>
+      </div>
+    </>
   )
 }
 

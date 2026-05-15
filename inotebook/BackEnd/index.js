734a4b8f8cf2c94c5b948c4610f664927740cc4e
Sintu4613S <shiva49767@gmail.com>
2026-05-15 10:31:30 +0530

    Add CORS middleware to enable cross-origin requests

--- inotebook/BackEnd/index.js
+++ inotebook/BackEnd/index.js
@@ -2,12 +2,16 @@ import express from 'express'
 import connectToMongo from './connectDb.js';
 import authRoutes from './routes/auth.js'
 import noteRoutes from './routes/note.js'
+import cors from 'cors'
 connectToMongo()
 
 const app = express()
 const port = 5000
+
+app.use(cors())
 // this is the method to parse the json data that we receive from the client and we can use this method to access the data in the req.body object.
 app.use(express.json())
+
 // this is the method to use the routes that we have defined in the routes/auth.js file and we can use this method to handle the requests that we receive from the client and return the response to the client.
 app.use('/api/auth', authRoutes)
 app.use('/api/note', noteRoutes)

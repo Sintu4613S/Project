ce83a6f855e831ccbd3c8ba56becea2e9153adb9
Sintu4613S <shiva49767@gmail.com>
2026-05-15 11:05:32 +0530

    Refactor Noteitem component to integrate context and handle note deletion

--- inotebook/src/components/Noteitem.jsx
+++ inotebook/src/components/Noteitem.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useContext } from "react"
+import notecontext from "../context/Notes/noteContext"
 
 const Noteitem = (props) => {
+  const context = useContext(notecontext)
+  // eslint-disable-next-line
+  const { deleteNote } = context
+  const HandleDelete = () => {
+    deleteNote(note._id)
+  }
+  // const HandleEdit = () => {
+
+  // }
   const { note } = props
   return (
     <>
-
       <div className="col-md-3 ">
         <div className="card my-3" >
           <div className="card-body">
@@ -12,10 +21,10 @@ const Noteitem = (props) => {
             <p className="card-text">{note.description}</p>
             <p className="card-text"><small className="text-muted">{note.tag}</small></p>
 
-            <span className="material-symbols-outlined icon">
+            <span className="material-symbols-outlined mx-2" onClick={HandleDelete}>
               delete
             </span>
-            <span className="material-symbols-outlined icon">
+            <span className="material-symbols-outlined mx-2">
               edit_square
             </span>
 

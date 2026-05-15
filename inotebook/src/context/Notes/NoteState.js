0ebb91ccf9e2f98d2760bdcf960c52d0a79f593a
Sintu4613S <shiva49767@gmail.com>
2026-05-15 11:05:49 +0530

    Implement note management functions: add, delete, and edit notes in NoteState component

--- inotebook/src/context/Notes/NoteState.js
+++ inotebook/src/context/Notes/NoteState.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Notecontext from "./noteContext"
 
 const Notestate = (props) => {
+  const host = "http://localhost:5000"
   const initialNotes = [
     {
       "_id": "69fc207aadb86e0c511299eb",
@@ -67,9 +68,55 @@ const Notestate = (props) => {
       "__v": 0
     }
   ]
-  const [state, setState] = useState(initialNotes)
+  const [note, setnote] = useState(initialNotes)
+
+  //Add a Note
+  const addNote = async (title, description, tag) => {
+    //Api call to edit a note
+    // eslint-disable-next-line
+    const response = await fetch(`${host}/api/note/getallnote`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuZXdVc2VyIjp7ImlkIjoiNjlmOGRkZWVjOWFkNDg2OGRhN2FhM2RjIn0sImlhdCI6MTc3NzkxNzQyMn0.9Esq-jVY29L19-XgDvIBv_NZj936_PoWpISycKTQvsM"
+      },
+      body: JSON.stringify({ title, description, tag })
+    });
+    //const result = await response.json();
+    const newNote = { _id: Date.now().toString(), title: title, description: description, tag: tag };
+    setnote([...note, newNote]);
+
+  };
+
+  //Delete a Note
+  const deleteNote = (_id) => {
+    setnote(note.filter((note) => note._id !== _id));
+  };
+  //Edit a Note
+  const editNote = async (id, title, description, tag) => {
+    //Api call to edit a note
+    const response = await fetch(`${host}/api/note/updatenote/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'auth-token': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuZXdVc2VyIjp7ImlkIjoiNjlmOGRkZWVjOWFkNDg2OGRhN2FhM2RjIn0sImlhdCI6MTc3NzkxNzQyMn0.9Esq-jVY29L19-XgDvIBv_NZj936_PoWpISycKTQvsM"
+      },
+      body: JSON.stringify({ title, description, tag })
+    });
+    const result = await response.json();
+
+    // Logic to edit a note
+    const newNotes = note.map((note) => {
+      if (note._id === id) {
+        return { ...note, title, description, tag }
+      }
+      return note;
+    });
+    setnote(newNotes);
+    return result;
+  }
   return (
-    <Notecontext.Provider value={{ notes: state, setNotes: setState }}>
+    <Notecontext.Provider value={{ notes: note, addNote, deleteNote, editNote }}>
       {props.children}
     </Notecontext.Provider>
 

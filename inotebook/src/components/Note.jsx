4468bb5a5b2cb8490a022582c56caf6e7da040cc
Sintu4613S <shiva49767@gmail.com>
2026-05-15 11:03:49 +0530

    Refactor Note component to log note IDs and remove unused setNotes

--- inotebook/src/components/Note.jsx
+++ inotebook/src/components/Note.jsx
@@ -1,19 +1,23 @@
 import React, { useContext } from "react"
 import notecontext from "../context/Notes/noteContext"
 import Noteitem from "./Noteitem"
+
 const Note = () => {
   const context = useContext(notecontext)
   // eslint-disable-next-line
-  const { notes, setNotes } = context
-
+  const { notes, addNote } = context
   console.log(notes)
+
   return (
-    <div className="row my-3">
-      <h2>Your Note</h2>
-      {notes.map((note) => {
-        return <Noteitem note={note} key={note._id} />
-      })}
-    </div>
+    <>
+      <div className="row my-3">
+        <h2>Your Note</h2>
+        {notes.map((note) => {
+          console.log(note._id)
+          return <Noteitem note={note} key={note._id} />
+        })}
+      </div>
+    </>
 
   )
 }

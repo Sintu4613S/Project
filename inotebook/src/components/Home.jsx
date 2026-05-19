4631098e146138453ba0fde10656f43553131455
Sintu4613S <shiva49767@gmail.com>
2026-05-19 10:05:45 +0530

    Add state management for modal visibility in Home component

--- inotebook/src/components/Home.jsx
+++ inotebook/src/components/Home.jsx
@@ -1,12 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import Note from "./Note";
 import AddNote from "./AddNote"
-
+import Modal from "./Modal"
 
 export default function Home() {
+  const [show, setShow] = useState(false)
+
   return (
     <>
       <AddNote />
+      <Modal show={show} onHide={() => setShow(false)} />
+
       <Note />
     </>
   )

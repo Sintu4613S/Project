a6c067fcc4b67b0fed00f12b2d2dfb46eda7b0a5
Sintu4613S <shiva49767@gmail.com>
2026-05-19 10:05:08 +0530

    Fetch notes on component mount and pass update function to Noteitem

--- inotebook/src/components/Note.jsx
+++ inotebook/src/components/Note.jsx
@@ -1,20 +1,31 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useRef } from "react"
 import notecontext from "../context/Notes/noteContext"
 import Noteitem from "./Noteitem"
 
 const Note = () => {
   const context = useContext(notecontext)
   // eslint-disable-next-line
-  const { notes, addNote } = context
-  console.log(notes)
+  const { notes, fetchAllNotes } = context
+  //console.log(notes)
+  useEffect(() => {
+    fetchAllNotes()
+    // eslint-disable-next-line
+  }, [])
+
+  const ref = useRef(null)
+  const updatenote = () => {
+    console.log("updete")
+    ref.current = ref.current.click()
+
+  }
 
   return (
     <>
+
       <div className="row my-3">
         <h2>Your Note</h2>
         {notes.map((note) => {
-          console.log(note._id)
-          return <Noteitem note={note} key={note._id} />
+          return <Noteitem note={note} key={note._id} updatenote={updatenote} />
         })}
       </div>
     </>

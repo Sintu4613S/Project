61a65b57955d656d48f0b17b0b4f0b423447d4ca
Sintu4613S <shiva49767@gmail.com>
2026-05-19 10:29:55 +0530

    Refactor the Home.jsx

--- inotebook/src/components/Home.jsx
+++ inotebook/src/components/Home.jsx
@@ -1,15 +1,13 @@
-import React, { useState } from "react"
+import React from "react"
 import Note from "./Note";
 import AddNote from "./AddNote"
-import Modal from "./Modal"
+
 
 export default function Home() {
-  const [show, setShow] = useState(false)
 
   return (
     <>
       <AddNote />
-      <Modal show={show} onHide={() => setShow(false)} />
 
       <Note />
     </>

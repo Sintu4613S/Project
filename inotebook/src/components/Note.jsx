91502e39aaf31bf3ce37974da8be85fb375f3e6b
Sintu4613S <shiva49767@gmail.com>
2026-05-19 12:31:47 +0530

    Enhance Note component with edit functionality and modal improvements

--- inotebook/src/components/Note.jsx
+++ inotebook/src/components/Note.jsx
@@ -1,10 +1,11 @@
-import React, { useContext, useEffect, useRef } from "react"
+import React, { useContext, useEffect, useRef, useState } from "react"
 import notecontext from "../context/Notes/noteContext"
 import Noteitem from "./Noteitem"
 const Note = () => {
   const context = useContext(notecontext)
   // eslint-disable-next-line
-  const { notes, fetchAllNotes } = context
+  const { notes, fetchAllNotes, editNote } = context
+  const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
   //console.log(notes)
   useEffect(() => {
     fetchAllNotes()
@@ -12,10 +13,23 @@ const Note = () => {
   }, [])
 
   const ref = useRef(null)
-  const updatenote = (note) => {
-    console.log("updete")
+  const refclose = useRef(null)
+  //eslint-disable-next-line
+  const updatenote = (curentNote) => {
+    setNote({ id: curentNote._id, etitle: curentNote.title, edescription: curentNote.description, etag: curentNote.tag })
     ref.current.click()
+  }
+  // eslint-disable-next-line
 
+  //eslint-disable-next-line
+  const handleClick = () => {
+    console.log("Updating the note...", note)
+    editNote(note.id, note.etitle, note.edescription, note.etag)
+    refclose.current.click()
+
+  }
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
   }
 
   return (
@@ -44,8 +58,9 @@ const Note = () => {
             <div className="modal-content">
               <div className="modal-header">
                 <h5 className="modal-title" id="exampleModalLongTitle">
-                  Modal title
+                  Edit Note
                 </h5>
+
                 <button
                   type="button"
                   className="btn-close"
@@ -53,31 +68,65 @@ const Note = () => {
                   aria-label="Close"
                 />
               </div>
-              <div className="modal-body">...</div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <button type="button" className="btn btn-primary">
-                  Save changes
-                </button>
+              <div className="modal-body">
+                <div className="mb-3">
+                  <label htmlFor="etitle" className="form-label">
+                    Title
+                  </label>
+                  <input type="text" className="form-control"
+                    id="etitle" name="etitle"
+                    value={note.etitle}
+                    onChange={onChange}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="edescription" className="form-label">
+                    Description
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="edescription" name="edescription"
+                    rows={2} style={{ resize: "none" }}
+                    value={note.edescription}
+                    onChange={onChange}
+                  />
+                  <div className="mb-3">
+                    <label htmlFor="etag" className="form-label">
+                      Tag
+                    </label>
+                    <input type="text" className="form-control"
+                      id="etag" name="etag"
+                      value={note.etag}
+                      onChange={onChange}
+                    />
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                    ref={refclose}
+                  >
+                    Close
+                  </button>
+                  <button type="button" className="btn btn-primary" onClick={handleClick}>
+                    Update Note
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="row my-3">
+          <h2>Your Note</h2>
+          {notes.map((note) => {
+            return <Noteitem note={note} key={note._id} updatenote={updatenote} />
+          })}
+        </div>
       </div>
-      <div className="row my-3">
-        <h2>Your Note</h2>
-        {notes.map((note) => {
-          return <Noteitem note={note} key={note._id} updatenote={updatenote} />
-        })}
-      </div>
-    </>
 
+    </>
   )
 }
 

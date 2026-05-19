4f4e89e2fb081d7c7f6710ba892de28b6a8e78b6
Sintu4613S <shiva49767@gmail.com>
2026-05-19 12:30:07 +0530

    Addin a Comment

--- inotebook/src/components/AddNote.jsx
+++ inotebook/src/components/AddNote.jsx
@@ -1,16 +1,22 @@
 import React, { useContext, useState } from "react"
 import notecontext from "../context/Notes/noteContext"
 const AddNote = () => {
+  // make  a context variable to access the context  values and function defined in the noteContext.jsx. useContext is a hook that allows us to access the context values and functions in a functional component.
   const context = useContext(notecontext)
   // eslint-disable-next-line
+  // Addig the note by calling the addNote function from the context and passing the title, description, and tag of the note as arguments and initial value is empty string.
   const { addNote } = context
   const [note, setNote] = useState({ title: "", description: "", tag: "" })
   // console.log(addNote)
   const handleAddNote = (e) => {
+    // e.preventDefault() is used to prevent the default behavior of the form submission, which is to reload the page. By calling e.preventDefault(), we can handle the form submission in our own way without triggering a page refresh.
     e.preventDefault()
+    // Here adding the new Note by calling the addNote function fromthe context and passing the title, description, and tag of the note as arguments. id,userand date are generated at the backend server.
     addNote(note.title, note.description, note.tag)
   }
+  // onChange is used to update the state of the note object whenever the user types in the input fields.
   const onChange = (e) => {
+    // setNote is used to update the state of the note object. It takes the current state of the note object and updates the specific property (title, description, or tag) based on the name attribute of the input field that triggered the onChange event.
     setNote({ ...note, [e.target.name]: e.target.value })
   }
   return (
@@ -22,6 +28,7 @@ const AddNote = () => {
 
       <div className="container my-3">
         <h2>Add a Note</h2>
+        // make a form to add a note with title, description and tag input fields and a submit button. onChange event is used to update the state of the note object whenever the user types in the input fields. handleAddNote function is called when the submit button is clicked to add the new note.
         <>
           <div className="mb-3">
             <label htmlFor="title" className="form-label">
@@ -54,12 +61,14 @@ const AddNote = () => {
                 onChange={onChange}
               />
             </div>
+
             <div className="col-12 my-2">
               <button className="btn btn-primary" type="submit" onClick={handleAddNote}>Submit</button>
             </div>
           </div>
         </>
       </div>
+
     </div>
   )
 }

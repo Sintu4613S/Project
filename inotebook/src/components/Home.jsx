8e1121c3bd592d43f2d2a8cb49d726af4052785c
Sintu4613S <shiva49767@gmail.com>
2026-05-19 12:30:47 +0530

    Adding A comment about note and addnote

--- inotebook/src/components/Home.jsx
+++ inotebook/src/components/Home.jsx
@@ -7,6 +7,7 @@ export default function Home() {
 
   return (
     <>
+    // calling the AddNote component to add a new note and Note component to display the list of notes.
       <AddNote />
 
       <Note />

e89aa32ee73795bd0de284d595ddce6f07c27aed
Sintu4613S <shiva49767@gmail.com>
2026-05-30 10:09:46 +0530

    nothing to commit

--- inotebook/src/components/Home.jsx
+++ inotebook/src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react"
 import Note from "./Note";
 import AddNote from "./AddNote"
 
-
 export default function Home() {
   // calling the AddNote component to add a new note and Notecomponent to display the list of notes.
   return (

5d6de888491f65e8161805654175145a54e39c23
Sintu4613S <shiva49767@gmail.com>
2026-05-30 08:58:56 +0530

    Refactor Home component comments for clarity

--- inotebook/src/components/Home.jsx
+++ inotebook/src/components/Home.jsx
@@ -4,12 +4,10 @@ import AddNote from "./AddNote"
 
 
 export default function Home() {
-
+  // calling the AddNote component to add a new note and Notecomponent to display the list of notes.
   return (
     <>
-    // calling the AddNote component to add a new note and Note component to display the list of notes.
       <AddNote />
-
       <Note />
     </>
   )
